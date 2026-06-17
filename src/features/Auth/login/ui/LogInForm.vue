fe280ef15c337290f559fb9e636932f680614916
Алексей Аверьянов <Flex_d1@mail.ru>
2026-06-17 20:50:02 +0300

    Валидация регистрации

--- src/features/Auth/login/ui/LogInForm.vue
+++ src/features/Auth/login/ui/LogInForm.vue
@@ -4,41 +4,49 @@ import { ButtonComponent } from '@/shared/ui/Button'
 import { InputComponent } from '@/shared/ui/Input'
 import { ref } from 'vue'
 
-const logInInput = ref('')
-const passwordInput = ref('')
-const logInEmpty = ref(false)
-const passwordEmpty = ref(false)
+const logInInput = ref<string>('')
+const passwordInput = ref<string>('')
+const logInEmpty = ref<boolean>(false)
+const passwordEmpty = ref<boolean>(false)
+const errorMessage = ref<string | null>('')
 
 const handleLogIn = async () => {
+  // если поля пустые
+  logInEmpty.value = !logInInput.value?.trim()
+  passwordEmpty.value = !passwordInput.value?.trim()
+
+  if (logInEmpty.value || passwordEmpty.value) {
+    errorMessage.value = 'Заполните все обязательные поля'
+    return
+  }
+
   try {
     await logIn({ identity: logInInput.value, password: passwordInput.value })
     console.log('Пользователь авторизован')
   } catch (error) {
-    console.log('Ошибка авторизации', error)
-    logInEmpty.value = true
-    passwordEmpty.value = true
+    if (error?.status === 400) {
+      errorMessage.value = 'Неверный логин или пароль'
+    } else {
+      errorMessage.value = 'Ошибка авторизации'
+    }
   }
 }
 </script>
 
 <template>
-  {{ logInInput }}
-  {{ passwordInput }}
-  <InputComponent
-    v-model:input-value="logInInput"
-    :input-type="'text'"
-    :input-required="logInEmpty"
-    :input-placeholder="'Ваш Логин'"
-    :error-message="null"
-  />
-
-  <InputComponent
-    v-model:input-value="passwordInput"
-    :input-type="'password'"
-    :input-required="passwordEmpty"
-    :input-placeholder="'Ваш пароль'"
-    :error-message="null"
-  />
-
-  <ButtonComponent :label="'Войти в систему'" @click="handleLogIn" />
+  <form @submit.prevent="handleLogIn">
+    <InputComponent
+      v-model:input-value="logInInput"
+      :input-required="logInEmpty"
+      :input-placeholder="'Ваш Логин'"
+    />
+    <InputComponent
+      v-model:input-value="passwordInput"
+      :input-type="'password'"
+      :input-required="passwordEmpty"
+      :input-placeholder="'Ваш пароль'"
+    />
+    <button type="submit">Авторизоваться</button>
+    {{ errorMessage }}
+  </form>
 </template>
--- src/features/Auth/registr/ui/RegistrForm.vue
+++ src/features/Auth/registr/ui/RegistrForm.vue
@@ -1,33 +1,110 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { registr } from '../api/registr.api'
-import { logIn } from '../@x/login'
+import { InputComponent } from '@/shared/ui/Input'
 
-const userName = ref('')
-const email = ref('')
-const password = ref('')
-const passwordConfirm = ref('')
+const userName = ref<string>('')
+const email = ref<string>('')
+const password = ref<string>('')
+const passwordConfirm = ref<string>('')
+const errorMessage = ref<string | null>('')
+const userNameEmpty = ref<boolean>(false)
+const emailEmpty = ref<boolean>(false)
+const passwordEmpty = ref<boolean>(false)
+const passwordConfirmEmpty = ref<boolean>(false)
 
 const onSubmit = async () => {
-  const response = await registr({
-    email: email.value,
-    password: password.value,
-    username: userName.value,
-    passwordConfirm: passwordConfirm.value,
-  })
+  userNameEmpty.value = !userName.value.trim()
+  emailEmpty.value = !email.value.trim()
+  passwordEmpty.value = !password.value.trim()
+  passwordConfirmEmpty.value = !passwordConfirm.value.trim()
 
-  console.log(response)
+  if (
+    userNameEmpty.value ||
+    emailEmpty.value ||
+    passwordEmpty.value ||
+    passwordConfirmEmpty.value
+  ) {
+    errorMessage.value = 'Заполните все обязательные поля'
+    return
+  }
+
+  try {
+    const response = await registr({
+      email: email.value,
+      password: password.value,
+      username: userName.value,
+      passwordConfirm: passwordConfirm.value,
+    })
+
+    // Нужно прокинуть в таб авторизации
+    console.log(response)
+  } catch (error: any) {
+    if (error?.response?.data) {
+      const errorData = error.response.data
+      console.log(errorData)
+
+      if (errorData.username) {
+        errorMessage.value =
+          'Неверное имя пользователя, пожалуйста введите имя на Латинице (Количество символов больше 3х)'
+        return
+      }
+      if (errorData.password) {
+        if (errorData.password.message === 'Must be at least 8 character(s).') {
+          errorMessage.value = 'Минимальное кличество символов для пароля 8'
+          return
+        }
+      }
+
+      if (errorData.passwordConfirm) {
+        if (errorData.passwordConfirm.code === 'validation_values_mismatch') {
+          errorMessage.value = 'Пароли не совпадают, проверьте правильность ввода'
+          return
+        }
+      }
+
+      if (errorData.email) {
+        if (errorData.email.code === 'validation_is_email') {
+          errorMessage.value = 'Направильно заполнено поле Email'
+          return
+        }
+        if (errorData.email.code === 'validation_not_unique') {
+          errorMessage.value = 'Пользователь с таким Email уже существует'
+          return
+        }
+      }
+    }
+  }
 }
 </script>
 
 <template>
   <form @submit.prevent="onSubmit">
-    <input type="text" name="userName" v-model="userName" />
-    <input type="text" name="email" v-model="email" />
-    <input type="text" name="passwrod" v-model="password" />
-    <input type="text" name="passwrod" v-model="passwordConfirm" />
-    <button type="submit">Зарегистрироваться</button>
-  </form>
+    <InputComponent
+      v-model:input-value="userName"
+      :input-required="userNameEmpty"
+      :input-placeholder="'Ваше Имя'"
+    />
+    <InputComponent
+      v-model:input-value="email"
+      :input-required="emailEmpty"
+      :input-placeholder="'Ваш Email'"
+    />
 
-  <button @click="handleLogIn">Авторизоваться</button>
+    <InputComponent
+      v-model:input-value="password"
+      :input-required="passwordEmpty"
+      :input-type="'password'"
+      :input-placeholder="'Пароль для входа'"
+    />
+
+    <InputComponent
+      v-model:input-value="passwordConfirm"
+      :input-required="passwordConfirmEmpty"
+      :input-type="'password'"
+      :input-placeholder="'Подтвердите пароль'"
+    />
+    <button type="submit">Зарегистрироваться</button>
+    {{ errorMessage }}
+  </form>
 </template>
